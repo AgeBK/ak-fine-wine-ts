74f0efe29a1be98b9f3e2350d84d8ad3ae716830
AgeBK <age24@hotmail.com>
2024-02-19 12:26:19 +1100

    gh-pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,14 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.headerRow}>
-        <img src="./AK.png" alt="test" />
+        <img src="./AK.png" alt="test" style="height:30px;width:30px" />
+        <img src="/AK.png" alt="test2" style="height:30px;width:30px" />
+        <img
+          src="./src/imgs/logos/AK.png"
+          alt="test3"
+          style="height:30px;width:30px"
+        />
+
         <div className={styles.logo}>
           <Link to="/">
             <Img
